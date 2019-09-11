5f71949a27009342dca673bd061b1d80f176c6e1
Bond Ragsdale <ber0003@mix.wvu.edu>
2019-09-11 02:09:40 -0400

    figured out the structure, using elif and helper functions. basically done except for content

--- spoilerFreeLexicon.py
+++ spoilerFreeLexicon.py
@@ -1,45 +1,45 @@
 #Bond Ragsdale, Devan Tighe, Kiersten Mikulsky, Bryan Stone, Nathan Smith
 
-# define the function blocks
-# def zero():
-#     print ("You typed zero.\n")
-
-# def sqr():
-#     print ("n is a perfect square\n")
-
-# def even():
-#     print ("n is an even number\n")
-
-# def prime():
-#     print ("n is a prime number\n")
-
-# # map the inputs to the function blocks
-# options = {0 : zero,
-#            1 : sqr,
-#            4 : sqr,
-#            9 : sqr,
-#            2 : even,
-#            3 : prime,
-#            5 : prime,
-#            7 : prime,
-# }
-
-
-characterName = ""
-chapter = ""
+characterNumber = 0
+chapter = 0
 
 def startFunction():
-    characterName = input("Please type the name of the character you want to learn about: ")
-    chapter = input("Which chapter are you up too? ")
-    print(characterName)
-    print(chapter)
+    print("1. Dante")
+    print("2. Virgil")
+    characterNumber = int(input("Please type the number of the character you want to learn about: "))
+    chapter = int(input("Which chapter are you up too? "))
+    getSummary(chapter, characterNumber)
+
+
+
+#writing helper functions for each character, that just prints a string based on the chapter 
+
+def danteFunction(chapter):
+    if chapter == 1:
+        print("dante up to ch 1")
+    elif chapter == 2:
+        print("dante up to ch 2")
+
+def virgilFunction(chapter):
+    if chapter == 1:
+        print("virgil chapter 1")
+    elif chapter == 2: 
+        print("virgil chapter 2")
+
+
+
+#getSummary takes in the values from the user, and matches the desired character, and then calls the corresponding helper function
+
+def getSummary(chapterNum, characterNum):
+    chapter = chapterNum
+    character = characterNum
+    
+    if characterNum == 1:
+        danteFunction(chapter)
+    elif characterNum == 2: 
+        virgilFunction(chapter)
+
 
 startFunction()
 
-if characterName == "Dante":
-    danteHelper()
 
-
-
-def danteHelper():
-    print("Dante is a cool dude")
