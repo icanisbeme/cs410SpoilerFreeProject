30797e9f1c2d6bd27a5973eab997ebde46b20d2c
Bond Ragsdale <ber0003@mix.wvu.edu>
2019-09-12 00:48:09 -0400

    added more content up to chapter 3

--- spoilerFreeLexicon.py
+++ spoilerFreeLexicon.py
@@ -6,6 +6,7 @@ chapter = 0
 def startFunction():
     print("1. Dante")
     print("2. Virgil")
+    print("3. Charon")
     characterNumber = int(input("Please type the number of the character you want to learn about: "))
     chapter = int(input("Which chapter are you up too? "))
     getSummary(chapter, characterNumber)
@@ -16,15 +17,21 @@ def startFunction():
 
 def danteFunction(chapter):
     if chapter == 1:
-        print("dante up to ch 1")
-    elif chapter == 2:
-        print("dante up to ch 2")
+        print("Dante finds himself in a dark wooded forest, walking alone and encountering wild beasts. He is then saved from the animals by Virgil")
+    elif chapter == 2 or chapter == 3:
+        print("Dante was in a wooded forest, until Virgil came to his aid. He has now progressed to the entrance of hell")
 
 def virgilFunction(chapter):
     if chapter == 1:
-        print("virgil chapter 1")
-    elif chapter == 2: 
-        print("virgil chapter 2")
+        print("Virgil comes to help Dante when he is lost in a wood. He is a deceased poet and exists now as a shade, and tells Dante that he will guide him through hell")
+    elif chapter == 2 or chapter == 3: 
+        print("Virgil has saved Dante and led him to the entrance of hell")
+
+def charonFunction(chapter):
+    if chapter < 3:
+        print("Invalid")
+    elif chapter >= 3:
+        print("Charon is the demon that brings people to hell across the river Acheron")
 
 
 
